cf59a720ada413732d9a97ac736af38f62a1e437
miltonc5 <miltn.3002@gmail.com>
2015-05-27 17:18:20 +0600

    mod

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(function() {
+
+	$('#diagram-id-1').circleDiagram({
+		"percent": "20%",
+		"size": "100",
+		"lineHeight": "70",
+		"borderWidth": "10",
+		"bgFill": "#64D9AF",
+		"frFill": "#F9CC4E",
+		"textSize": "20",
+		"textColor": "#585858"
+	});
+
+	$('#diagram-id-2').circleDiagram({
+		"percent": "50%",
+		"size": "100",
+		"lineHeight": "70",
+		"borderWidth": "10",
+		"bgFill": "#64D9AF",
+		"frFill": "#F9CC4E",
+		"textSize": "20",
+		"textColor": "#585858"
+	});
+	
+	$('#diagram-id-3').circleDiagram({
+		"percent": "80%",
+		"size": "100",
+		"lineHeight": "70",
+		"borderWidth": "10",
+		"bgFill": "#64D9AF",
+		"frFill": "#F9CC4E",
+		"textSize": "20",
+		"textColor": "#585858"
+	});
+	
+	$('#diagram-id-4').circleDiagram({
+		"percent": "100%",
+		"size": "100",
+		"lineHeight": "70",
+		"borderWidth": "10",
+		"bgFill": "#64D9AF",
+		"frFill": "#F9CC4E",
+		"textSize": "20",
+		"textColor": "#585858"
+	});
+});
